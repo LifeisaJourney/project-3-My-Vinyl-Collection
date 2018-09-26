719cf6ce093cb4201d5bbe67ba2f0bd89dd168fd
Alice Houel <amhouel@gmail.com>
2018-09-26 16:16:55 -0400

    bcrypt and jwtwebtoken packages added to package.json and required in server.js. get/api/albums route created.

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {User, Album, UserAlbums} = require('./models');
 const PORT = process.env.PORT || 5678;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const jwtSecret = 'secret189230';
+
+app.get('/api/albums', async(req,res) => {
+  const allAlbums = await Album.findAll({});
+  res.json(allAlbums);
+});
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
