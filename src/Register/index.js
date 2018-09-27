d0bc3d9ae57a65e761baeb639b761235b7314666
LifeH <hwie.zhu@gmail.com>
2018-09-27 15:46:17 -0400

    added register button and register component, can render page

--- src/Register/index.js
+++ src/Register/index.js
@@ -14,17 +14,63 @@ export default class Register extends Component {
   }
   register = async () => {
     const requestBody = JSON.stringify({
-
-
-      
+      name: this.state.name,
+      username: this.state.username,
+      password: this.state.password,
+      pictureSrc: this.state.pictureSrc,
+      city: this.state.city
+    });
+    const response = await fetch('/api/register', {
       method: 'POST',
-      body: 
+      body: requestBody, 
+      headers: { 
+        'Content-Type': 'application/json'
+      }
+    });
+    const responseBody = await response.json();
+    if (response.status===409) {
+      this.setState({
+        errorMessage: responseBody.message
+      });
+      return;
+    }
+    this.props.onLogIn();
+    localStorage.setItem('user-jwt', JSON.stringify(responseBody.token));
+  }
+
+  onInputChange = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value
     })
   }
+  onFormSubmit = evt => {
+    evt.preventDefault();
+  }
+
+
   render() {
     return (
       <div>
-        
+        <form onSubmit={this.onFormSubmit}>
+          <label>Name: </label>
+          <input type='text' placeholder='Name' onChange={this.onInputChange} name='Name' value={this.state.name}>
+          </input>
+          <label>User name: </label>
+          <input type='text' placeholder='User name' onChange={this.onInputChange} name='User name' value={this.state.username}>
+          </input>
+          <label>Password: </label>
+          <input type='password' placeholder='Password' onChange={this.onInputChange} name='Password' value={this.state.password}> 
+          </input>
+          <label>Profile Picture </label>
+          <input type='file' onChange={this.onInputChange} name='Avatar' accept='.png, .jpg, .jpeg' value={this.state.pictureSrc}>
+          </input>
+          <label>City: </label>
+          <input type='text' placeholder='City' onChange={this.onInputChange} name='City' value={this.state.city}>
+          </input>
+          <button type='button' onClick={this.register}>
+           Register 
+          </button>   
+        </form>
       </div>
     )
   }
